115f33bd30f1224d1f81da8c2403e2fece3e74cc
Xavi Torelló <info@xaviertorello.cat>
2016-02-29 19:24:57 +0100

    Repo structure updated in order to solve #1

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = ['workalendar', 'isoweek']
+
+setup(
+    name='one_year_ago',
+    description='Easy way to get the same day scenario (week, weekday, working day and holiday) for a previous year.',
+    version='0.0.2',
+    url='https://www.gisce.net',
+    author='GISCE Enginyeria, SL',
+    author_email='devel@gisce.net',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    license='General Public Licence 3',
+    provides=['one_year_ago']
+)

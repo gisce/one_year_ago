1fb4316d63b466f4d69c646450ad5407c43b818a
Xavi Torelló <info@xaviertorello.cat>
2016-09-20 17:26:55 +0200

    Added dependancy electrical_calendar and v0.5.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-INSTALL_REQUIRES = ['workalendar', 'isoweek']
+INSTALL_REQUIRES = ['workalendar', 'isoweek', 'electrical_calendar']
 
 setup(
     name='one_year_ago',
     description='Easy way to get the same day scenario (week, weekday, working day and holiday) for a previous year.',
-    version='0.5',
+    version='0.5.1',
     url='https://www.gisce.net',
     author='GISCE Enginyeria, SL',
     author_email='devel@gisce.net',
